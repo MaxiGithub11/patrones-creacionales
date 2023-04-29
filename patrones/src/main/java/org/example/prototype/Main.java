e357088c3bb2e3a24b2bbe3092e0029a1e202d4b
Maxi <maxi.ush.ind@gmail.com>
2023-04-28 21:47:47 -0300

    modificacion sout

--- patrones/src/main/java/org/example/prototype/Main.java
+++ patrones/src/main/java/org/example/prototype/Main.java
@@ -3,5 +3,6 @@ package org.example.prototype;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Main del Maxi");
+        System.out.println("holis");
     }
 }
